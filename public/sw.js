057e8c9e292abfa271cb0a8f9a38446f4074f6d4
Willie2728 <Willie2728@users.noreply.github.com>
2026-09-09 02:32:00 -0700

    Remove automatic intro dialog

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "avery-clubhouse-v1";
+const CACHE = "avery-clubhouse-v2";
 const APP_SHELL = ["/", "/manifest.webmanifest", "/assets/icon.svg", "/assets/lounge.png", "/assets/willie-avatar.webp"];
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
